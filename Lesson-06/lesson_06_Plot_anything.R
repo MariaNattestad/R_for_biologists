98eb3b59698ceb99ec52c9c85c88c5d99f2260e0
MariaNattestad <lunexa@gmail.com>
2015-09-13 20:17:55 -0400

    Finish lesson 6--plot anything

--- Lesson-06/lesson_06_Plot_anything.R
+++ Lesson-06/lesson_06_Plot_anything.R
@@ -1,6 +1,6 @@
 # ==========================================================
 #
-#                         In Progress
+#               New data set for plotting: variants
 #
 # ==========================================================
 
@@ -55,15 +55,110 @@ ggplot(my_data, aes(x=type,y=size,fill=type)) + geom_boxplot() + coord_flip()
 # Creating violin plots
 ggplot(my_data, aes(x=type,y=size)) + geom_violin()
 ggplot(my_data, aes(x=type,y=size,fill=type)) + geom_violin() + ylim(0,1000) + guides(fill=FALSE)
-ggplot(my_data, aes(x=type,y=size,fill=type)) + geom_violin(adjust=0.5) + ylim(0,1000) 
+ggplot(my_data, aes(x=type,y=size,fill=type)) + geom_violin(adjust=0.5) + ylim(0,1000)
     # default adjust is 1, lower means finer resolution
 
 # You can log-scale any numerical axis on any plot
 ggplot(my_data, aes(x=type,y=size,fill=type)) + geom_violin() + scale_y_log10()
 
+# Creating a density plot
+ggplot(my_data, aes(x=size,fill=type)) + geom_density() + xlim(0,500)
+    # similar to this histogram:
+ggplot(my_data, aes(x=size,fill=type)) + geom_bar(binwidth=5) + xlim(0,500)
+
+ggplot(my_data, aes(x=size,fill=type)) + geom_density(position="stack") + xlim(0,500)
+ggplot(my_data, aes(x=size,fill=type)) + geom_density(alpha=0.5) + xlim(0,500)
+
+# Sneak peak at Lesson 7: multifaceted plots:
+ggplot(my_data, aes(x=size,fill=type)) + geom_density(alpha=0.5) + xlim(0,500) + facet_grid(type ~ .)
+
+
 # Creating dot plots
+ggplot(my_data, aes(x=size,fill=type)) + geom_dotplot()
+    # makes more sense with fewer observations where each individual item matters, 
+    # so let's grab the largest events and see what chromosomes they fall on
+large_data <- my_data[my_data$size>5000,]
+ggplot(large_data, aes(x=size,fill=type)) + geom_dotplot(method="histodot")
+# careful, they don't stack automatically, so some of the dots are behind others
+ggplot(large_data, aes(x=size,fill=type)) + geom_dotplot(method="histodot",stackgroups=TRUE)
 
 
 
+
+# Time-course data for line plot
+filename <- "Lesson-06/time_course_data.txt"
+time_course <- read.csv(filename, sep=",", quote='', stringsAsFactors=TRUE,header=TRUE)
+time_course
+
+# line plot:
+ggplot(time_course, aes(x=seconds,y=value,colour=sample)) + geom_line()
+ggplot(time_course, aes(x=seconds,y=value,colour=sample)) + geom_line(size=3)
+
+
+# For fun:
+# Any plot can be made in polar coordinates:
+ggplot(time_course, aes(x=seconds,y=value,colour=sample)) + geom_line(size=3) + coord_polar()
+ggplot(my_data, aes(x=type,y=size,fill=type)) + geom_violin(adjust=0.5) + ylim(0,1000) + coord_polar()
+ggplot(my_data, aes(x=size,fill=type)) + geom_bar(binwidth=5) + xlim(0,500) + coord_polar()
+
+
+
+# Pie charts
+
+# While ggplot doesn't come with pie charts, we can hack one together:
+# This function makes it happen:
+pie_with_ggplot <- function(my_data,type) {
+    # inspired by mathematicalcoffee.blogspot.com/2014/06/ggpie-pie-graphs-in-ggplot2.html
+    
+    counts <- summary(my_data[,type])
+    
+    ggplot(my_data, aes(x=factor(1),fill=type)) + geom_bar(width=1) + coord_polar(theta="y") + 
+        # special theme for pie charts
+        theme(axis.title = element_blank(), 
+              axis.text.y = element_blank(), 
+              axis.ticks=element_blank(), 
+              panel.grid.major = element_blank(), 
+              panel.grid.minor = element_blank(), 
+              panel.background=element_blank(),
+              axis.text.x = element_text(color="black")
+              #               plot.margin = unit(rep(-2,4),"lines")
+        ) + 
+        # labels
+        scale_y_continuous(breaks=cumsum(counts)-counts/2, labels=names(counts)) + 
+        # remove legend
+        guides(fill=FALSE)
+}
+
+# but it's really simple to just run that function
+png(file="Lesson-06/pie.png",width=800,height=800)
+pie_with_ggplot(my_data,"type")
+dev.off()
+
+
+
+
+# Gene lists for Venn Diagram
+listA <- read.csv("Lesson-06/genes_list_A.txt",header=FALSE)
+A <- listA$V1
+
+listB <- read.csv("Lesson-06/genes_list_B.txt",header=FALSE)
+B <- listB$V1
+
+listC <- read.csv("Lesson-06/genes_list_C.txt",header=FALSE)
+C <- listC$V1
+
+listD <- read.csv("Lesson-06/genes_list_D.txt",header=FALSE)
+D <- listD$V1
+
+
+
+# install package VennDiagram
+library(VennDiagram)
+# This function only works by saving directly to a file
+venn.diagram(list("list C"=C, "list D"=D), fill = c("yellow","cyan"), cex = 1.5, filename="Lesson-06/Venn_diagram_genes_2.png")
+
+venn.diagram(list(A = A, C = C, D = D), fill = c("yellow","red","cyan"), cex = 1.5,filename="Lesson-06/Venn_diagram_genes_3.png")
+
+venn.diagram(list(A = A, B = B, C = C, D = D), fill = c("yellow","red","cyan","forestgreen"), cex = 1.5,filename="Lesson-06/Venn_diagram_genes_4.png")
 
 
